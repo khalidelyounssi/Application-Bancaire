14997a47f6899aabc2c22d531f6314531025b64d
khalidelyounssi <elyounssiox2000@gmail.com>
2025-12-26 09:35:36 +0100

    fixed cliein repo

--- src/Repositories/ClientRepository.php
+++ src/Repositories/ClientRepository.php
@@ -10,11 +10,38 @@ class ClientRepository {
     }
 
     public function add(Client $c) {
+        $checkSql = "SELECT COUNT(*) FROM clients WHERE email = ?";
+        $stmtCheck = $this->pdo->prepare($checkSql);
+        $stmtCheck->execute([$c->getEmail()]);
+      $existe = $stmtCheck->fetchColumn(); 
+
+        if ($existe > 0) {
+            echo "⚠️ Erreur :Email dija existe\n";
+            return;
+        }
+
         $sql = "INSERT INTO clients (nom, prenom, email, telephone) VALUES (?, ?, ?, ?)";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([$c->getNom(), $c->getPrenom(), $c->getEmail(), $c->getTelephone()]);
         
         $c->setId($this->pdo->lastInsertId());
+        echo "✅ succes !\n";
+    }
+
+    public function delete($clientId) {
+        $checkSql = "SELECT COUNT(*) FROM comptes WHERE client_id = ?";
+        $stmtCheck = $this->pdo->prepare($checkSql);
+        $stmtCheck->execute([$clientId]);
+        
+        $nbComptes = $stmtCheck->fetchColumn();
+
+        if ($nbComptes > 0) {
+            echo "impossible de supprimer\n";
+        } else {
+            $sql = "DELETE FROM clients WHERE id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$clientId]);
+        }
     }
 
     public function getAll() {
